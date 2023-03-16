c29c034ca77278bfb02f3833919fa32997c185de
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-03-15 14:16:32 -1000

    lab3

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import data from './data/data.json';
+// import data from './data/data.json';
 import './Main.css';
 
 class Main extends React.Component {
   render() {
 
     return (
-      // <>
-      //   <HornedBeast
-      //    title= {'Fred'} 
-      //    imageURL= {'fred.com'} description={'red'}></HornedBeast>
-      //   <HornedBeast 
-      //   title= {'Gus'} 
-      //   imageURL= {'gus.com'} 
-      //   description={'blue'}></HornedBeast>
-
-      // </>
       <main>
-        {data.map((beastObj, index) => {
-          return <HornedBeast _id={beastObj._id} image_url={beastObj.image_url} title={beastObj.title} description={beastObj.description} keyword={beastObj.keyword} horns={beastObj.horns} key={index}/>
-        })
-        }
+        {this.props.data.map((beastObj, index) => {
+          return (
+            <HornedBeast
+              handleRenderModal={this.props.handleRenderModal}
+              _id={beastObj._id}
+              image_url={beastObj.image_url}
+              title={beastObj.title}
+              description={beastObj.description} keyword={beastObj.keyword}
+              horns={beastObj.horns}
+              key={index}
+            />
+          )
+        })}
       </main>
     )
   }
